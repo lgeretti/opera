76a1059fc567f96c25794b00e6ca0d969088ca11
Luca Geretti <luca.geretti@gmail.com>
2021-07-23 14:56:47 +0200

    Supply base files for the project

---
+++ src/opera.cpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *            opera.cpp
+ *
+ *  Copyright  2021  Luca Geretti
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <ariadne.hpp>
+
+using namespace Ariadne;
+
+Int main(Int argc, const char* argv[]) {
+    if (not CommandLineInterface::instance().acquire(argc, argv)) return -1;
+}
