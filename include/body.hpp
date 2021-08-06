c78b3ddfd5d0d2b8c070af5cff37dd264fe0e060
Luca Geretti <luca.geretti@gmail.com>
2021-08-06 20:17:39 +0200

    Fix linking due to not enclosing implementation in namespace Opera

--- include/body.hpp
+++ include/body.hpp
@@ -75,72 +75,7 @@ class BodySegmentOccupancy {
 };
 
 //! \brief Calculate the minimum distance between two segments
-inline FloatType distance(BodySegmentOccupancy const& s1, BodySegmentOccupancy const& s2) {
-
-    const FloatType SMALL_VALUE(0.000001,Ariadne::dp);
-
-    auto u = s1.tail_position() - s1.head_position();
-    auto v = s2.tail_position() - s2.head_position();
-    auto w = s1.head_position() - s2.head_position();
-
-    FloatType a = dot(u, u);
-    FloatType b = dot(u, v);
-    FloatType c = dot(v, v);
-    FloatType d = dot(u, w);
-    FloatType e = dot(v, w);
-    FloatType D = a * c - b * b;
-    FloatType sc = D, sN = D, sD = D;
-    FloatType tc = D, tN = D, tD = D;
-    if (decide(D < SMALL_VALUE)) {
-        sN = 0;
-        sD = 1;
-        tN = e;
-        tD = c;
-    } else {
-        sN = (b * e - c * d);
-        tN = (a * e - b * d);
-        if (decide(sN < 0)) {
-            sN = 0;
-            tN = e;
-            tD = c;
-        } else if (decide(sN > sD)) {
-            sN = sD;
-            tN = e + b;
-            tD = c;
-        }
-    }
-    if (decide(tN < 0)) {
-        tN = 0;
-        if (decide(-d < 0)) {
-            sN = 0;
-        } else if (decide(-d > a)) {
-            sN = sD;
-        } else {
-            sN = -d;
-            sD = a;
-        }
-    } else if (decide(tN > tD)) {
-        tN = tD;
-        if (decide((-d + b) < 0)) {
-            sN = 0;
-        } else if (decide((-d + b) > a)) {
-            sN = sD;
-        } else {
-            sN = (-d + b);
-            sD = a;
-        }
-    }
-
-    if (decide(abs(sN) < SMALL_VALUE)) sc = 0;
-    else sc = sN / sD;
-
-    if (decide(abs(tN) < SMALL_VALUE)) tc = 0;
-    else tc = tN / tD;
-
-    auto dP = w + (sc * u) - (tc * v);
-
-    return sqrt(dot(dP, dP));
-}
+FloatType distance(BodySegmentOccupancy const& s1, BodySegmentOccupancy const& s2);
 
 }
 
--- src/body.cpp
+++ src/body.cpp
@@ -24,7 +24,7 @@
 
 #include "body.hpp"
 
-using namespace Opera;
+namespace Opera {
 
 BodySegment::BodySegment(unsigned int head_id, unsigned int tail_id, FloatType const& thickness) :
 _head_id(head_id), _tail_id(tail_id), _thickness(thickness) { }
@@ -68,4 +68,73 @@ bool BodySegmentOccupancy::intersects(BodySegmentOccupancy const& other) const {
     else {
         return (decide(distance(*this,other) <= _segment->thickness()+other._segment->thickness()));
     }
+}
+
+FloatType distance(BodySegmentOccupancy const& s1, BodySegmentOccupancy const& s2) {
+
+    const FloatType SMALL_VALUE(0.000001,Ariadne::dp);
+
+    auto u = s1.tail_position() - s1.head_position();
+    auto v = s2.tail_position() - s2.head_position();
+    auto w = s1.head_position() - s2.head_position();
+
+    FloatType a = dot(u, u);
+    FloatType b = dot(u, v);
+    FloatType c = dot(v, v);
+    FloatType d = dot(u, w);
+    FloatType e = dot(v, w);
+    FloatType D = a * c - b * b;
+    FloatType sc = D, sN = D, sD = D;
+    FloatType tc = D, tN = D, tD = D;
+    if (decide(D < SMALL_VALUE)) {
+        sN = 0;
+        sD = 1;
+        tN = e;
+        tD = c;
+    } else {
+        sN = (b * e - c * d);
+        tN = (a * e - b * d);
+        if (decide(sN < 0)) {
+            sN = 0;
+            tN = e;
+            tD = c;
+        } else if (decide(sN > sD)) {
+            sN = sD;
+            tN = e + b;
+            tD = c;
+        }
+    }
+    if (decide(tN < 0)) {
+        tN = 0;
+        if (decide(-d < 0)) {
+            sN = 0;
+        } else if (decide(-d > a)) {
+            sN = sD;
+        } else {
+            sN = -d;
+            sD = a;
+        }
+    } else if (decide(tN > tD)) {
+        tN = tD;
+        if (decide((-d + b) < 0)) {
+            sN = 0;
+        } else if (decide((-d + b) > a)) {
+            sN = sD;
+        } else {
+            sN = (-d + b);
+            sD = a;
+        }
+    }
+
+    if (decide(abs(sN) < SMALL_VALUE)) sc = 0;
+    else sc = sN / sD;
+
+    if (decide(abs(tN) < SMALL_VALUE)) tc = 0;
+    else tc = tN / tD;
+
+    auto dP = w + (sc * u) - (tc * v);
+
+    return sqrt(dot(dP, dP));
+}
+
 }
