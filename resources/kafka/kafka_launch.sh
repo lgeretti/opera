a38f822ff2a113b1d3e494a537c9867e7a88f4e5
andreagagliardo96 <andrea.gagliardo96@gmail.com>
2021-09-14 16:56:49 +0200

    Changes to be committed: 	modified:   resources/kafka/kafka_launch.sh 	new file:   resources/kafka/server.properties 	new file:   resources/kafka/zookeeper.properties

--- resources/kafka/kafka_launch.sh
+++ resources/kafka/kafka_launch.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo Launching kafka test system
+pwd
 cd
-cd "$(find -name "kafka_2.13-2.8.0" 2>/dev/null)"
-terminator -e "./bin/zookeeper-server-start.sh config/zookeeper.properties" -p hold
+cd "$(find ~ -path "*opera/resources/kafka")"
+
+terminator -e "source ~/.bashrc && zookeeper-server-start.sh zookeeper.properties | sleep 10" -p hold
 sleep 2
-terminator -e "./bin/kafka-server-start.sh config/server.properties" -p hold
+terminator -e "source ~/.bashrc && kafka-server-start.sh server.properties | sleep 10" -p hold
 echo Started server and broker
 echo Creating the 3 topics
 sleep 2
-terminator -e "./bin/kafka-topics.sh --create --topic opera-presentation --bootstrap-server localhost:9092"
-terminator -e "./bin/kafka-topics.sh --create --topic opera-state --bootstrap-server localhost:9092"
-terminator -e "./bin/kafka-topics.sh --create --topic opera-collision-notification --bootstrap-server localhost:9092"
+terminator -e "kafka-topics.sh --create --topic opera-presentation --bootstrap-server localhost:9092"
+terminator -e "kafka-topics.sh --create --topic opera-state --bootstrap-server localhost:9092"
+terminator -e "kafka-topics.sh --create --topic opera-collision-notification --bootstrap-server localhost:9092"
 sleep 0.5
 echo Topic created:
 bin/kafka-topics.sh --list --bootstrap-server localhost:9092
